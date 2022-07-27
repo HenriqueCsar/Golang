3580de555053c50e6dbed4c94c3c3e14b08ba133
Henrique Silva <henrique.fibonacci@hotmail.com>
2022-07-26 22:56:00 -0300

    Atualização HTTP com renderizador

--- HTTP/http.go
+++ HTTP/http.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
 func main() {
 
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	// SEM RENDERIZAÇÃO DE HTML
+	// http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Olá Mundo!"))
+	// })
+
+	//COM RENDEZIDAÇÃO HTML
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Olá Mundo!"))
+		u := usuario{
+			"João",
+			"João.pedro@hotmail.com",
+		}
+
+		templates.ExecuteTemplate(w, "home.html", u)
 	})
 
+	fmt.Println("Listen on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
